d62df19d97b2f08ef4ea243c56e218a52ea4953e
Gurtaj <gurtajs98@gmail.com>
2024-05-10 10:24:33 +0100

    completed ticket 10

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import {Routes, Route} from 'react-router-dom'
 import Header from './components/Header'
 import Home from './components/Home'
 import SingleArticle from './components/SingleArticle'
+import Topic from './components/Topics'
+import ArticleByTopic from './components/ArticleByTopic'
 
 function App() {
 
@@ -13,6 +15,7 @@ function App() {
       <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/article/:article_id" element={<SingleArticle/>} />
+      <Route path="/article/topic/:topic" element={<ArticleByTopic/>}/>
       </Routes>
     </div>
   )
---
+++ src/components/Topics.jsx
@@ -0,0 +1,40 @@
+import { useSearchParams, Link } from "react-router-dom";
+import {useState, useEffect} from 'react'
+import { getTopics } from "../../api";
+
+
+function Topics() {
+  
+    const [topics, setTopics] = useState([]);
+
+   
+
+    useEffect(() => {
+      getTopics().then((data) => {
+        setTopics(data);
+      });
+    }, []);
+
+    return (
+        <>
+        <h1>View by topic:</h1>
+        <div className="flex gap-10">
+        {topics.map((topic) => {
+          return (
+            <div className="border-2 border-black rounded-md p-2">
+              <Link to={`/article/topic/${topic.slug}`}>
+                <p className="font-bold" >{topic.slug[0].toUpperCase()+topic.slug.slice(1)}</p>
+                <p>{topic.description}</p>
+              </Link>
+            </div>
+          );
+        })}
+        {/* <Link to={`/article/${topic}`}><button className="border-2 border-black rounded-md">{topic}</button></Link>
+        <button className="border-2 border-black rounded-md">Coding</button>
+        <button className="border-2 border-black rounded-md">Football</button> */}
+      </div>
+      </>
+    )
+}
+
+export default Topics
