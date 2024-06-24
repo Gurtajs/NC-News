6ee2d611cf4d8daa868436c81f01f744e5504687
Gurtaj <gurtajs98@gmail.com>
2024-06-24 10:36:06 +0100

    Added loading gif

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,15 +2,17 @@ import Article from "./Articles";
 import Topics from "./Topics";
 import SortArticles from "./SortAllArticles";
 import PostArticle from "./PostArticle";
+import loader from "../assets/loading.gif"
 
 function Home({ articles, setArticles, loading }) {
   if (loading) {
     return (
-      <>
+      <div className="flex flex-col items-center">
         <h2>
           The articles are loading, thank you for your patience!
         </h2>
-      </>
+        <img src={loader} alt="loader" width={100} height={100}/>
+      </div>
     );
   }
 
